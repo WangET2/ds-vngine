dca36260bb44b7d085cd1571230c9e425b02602c
WangET2 <etwang2@gmail.com>
2026-04-28 21:29:06 -0700

    WIP: Implement CHOICE dispatch.

--- tests/test_interpreter.c
+++ tests/test_interpreter.c
@@ -327,7 +327,7 @@ void run_interpreter_tests(void){
 
     RUN_TEST(test_interpreter_choice);
     RUN_TEST(test_interpreter_choice_dispatch);
-    //RUN_TEST(test_interpreter_choice_nested_dispatch);
+    RUN_TEST(test_interpreter_choice_nested_dispatch);
 
     RUN_TEST(test_interpreter_pass);
     RUN_TEST(test_interpreter_wait);
