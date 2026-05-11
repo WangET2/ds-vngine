891d522511ea9789c7bdbd29dbce1102e6478dc4
WangET2 <etwang2@gmail.com>
2026-05-10 23:58:21 -0700

    Updated build process to generate soundbank table for audio instructions.

--- build.py
+++ build.py
@@ -5,17 +5,24 @@
 # SPDX-FileContributor: Antonio Niño Díaz, 2024
 
 from architectds import *
+from tools.soundbank_mapgen import create_soundbank_gperf, build_soundbank_source
 
 nitrofs = NitroFS()
 nitrofs.add_grit(['assets/graphics'])
 nitrofs.add_files_unchanged(['assets/scripts'], out_dir='scripts')
 nitrofs.add_files_unchanged(['assets/graphics/sprites/offsets'], out_dir='offsets')
+nitrofs_soundbank_header = nitrofs.add_mmutil(['assets/audio'])
 nitrofs.generate_image()
 
 arm9 = Arm9Binary(
     sourcedirs=['src'],
+    libs=['nds9', 'mm9'],
+    libdirs=['${BLOCKSDS}/libs/libnds', '${BLOCKSDS}/libs/maxmod'],
     includedirs=['include']
 )
+arm9.add_header_dependencies([nitrofs_soundbank_header])
+create_soundbank_gperf()
+build_soundbank_source()
 arm9.generate_elf()
 
 nds = NdsRom(
