11e07fbd982d89d98eb47cba17ac85a4cdc4013a
WangET2 <etwang2@gmail.com>
2026-05-12 03:27:05 -0700

    Prevent asset conversion from running when cleaning build artifacts.

--- build.py
+++ build.py
@@ -8,10 +8,12 @@ from architectds import *
 from tools.soundbank_mapgen import create_soundbank_gperf, build_soundbank_source
 from tools.backgroundmaker import convert_backgrounds
 from tools.spritemaker import convert_sprites
+import sys
 
 nitrofs = NitroFS()
-convert_sprites()
-convert_backgrounds()
+if len(sys.argv) == 1:
+    convert_sprites()
+    convert_backgrounds()
 nitrofs.add_grit(['build/assets/graphics', 'assets/graphics/ui'])
 nitrofs.add_grit(['assets/graphics/ui'],out_dir='grit/ui')
 nitrofs.add_files_unchanged(['assets/scripts'], out_dir='scripts')
@@ -26,9 +28,9 @@ arm9 = Arm9Binary(
     includedirs=['include']
 )
 arm9.add_header_dependencies([nitrofs_soundbank_header])
-create_soundbank_gperf()
-build_soundbank_source()
 arm9.generate_elf()
+
+
 
 nds = NdsRom(
     binaries=[arm9, nitrofs],
@@ -38,3 +40,7 @@ nds = NdsRom(
 nds.generate_nds()
 
 nds.run_command_line_arguments()
+
+if len(sys.argv) == 1:
+    create_soundbank_gperf()
+    build_soundbank_source()
